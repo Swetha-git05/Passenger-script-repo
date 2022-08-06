0e81a9437ded50034c95fe973500798ab169442e
P.Swetha <91486212+Swetha-git05@users.noreply.github.com>
2022-08-06 22:30:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ let num1 = 8
 let num2 = 2
 document.getElementById("num1-el").textcontent = num1
 document.getElementById("num2-el").textcontent = num2
+let sumEl = document.getElementById("sum-el")
+function add(){
+let result = mun1 + num2
+sumEl.textContent = "sum: " + result
+}
+function Subtract(){
+let result = mun1 - num2
+sumEl.textContent = "sum: " + result
+}
+function Multiply(){
+let result = mun1 * num2
+sumEl.textContent = "sum: " + result
+}
+function Divide(){
+let result = mun1 / num2
+sumEl.textContent = "sum: " + result
+}
