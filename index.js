d8033d4896533911d863580b1a12c0fd55784236
P.Swetha <91486212+Swetha-git05@users.noreply.github.com>
2022-08-07 00:34:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,132 @@ function Divide(){
 let result = mun1 / num2
 sumEl.textContent = "sum: " + result
 }
+
+
+
+Objects and Functions
+==================================================
+Create a person that contains three keys:name,age, and country
+create a function logdata(),--->this is used to craete aString format  
+
+let person = {
+name:"per",
+age: 35,
+country:"norwat"
+}
+function logdata(){
+console.log(person.name + " is " +  person.age + "years old and linves in" + person.country)
+}
+logdata()
+
+-----> If else
+====================================
+if(age < 6)
+{
+console.log("free")
+}
+else if (age < 18 ){
+console.log("child discount")
+}
+else if ( age < 27 )
+console.log("student discount")
+}
+else if(age < 67){
+console.log("full prize")
+}
+else {
+console.log("senior citizen discount")
+}
+
+
+----->Loops and Arrays
+=======================================
+let largecountries = ["china", "india", "usa", "Indonesia", "pakistan"]
+for(let i = 0; i < largecountries.length: i++){
+console.log("-" + largecountries[i])
+}
+
+----->pop, unshift, push, shiftchallenge
+======================================
+let largecountries = ["canada", "India","usa","indonesia","monaco"]
+largecountries.pop()
+largecountries.push("pakisthan")
+largecountries.shift()
+console.log(largecountries)
+
+output
+=======
+["canada", "India","usa","indonesia", "pakisthan"]
+
+shift  ------   output
+=========
+ ["India","usa","indonesia", "pakisthan"] //canada is removed
+
+unshift :  means removed "canada" will be included in the same place
+=======
+
+logical operators
+==================
+let dayOfMonth = 13
+let weekday = "friday"
+logical AND operater ----> &&
+if ( dayOfMonth ===13 && weekday ==="friday"){
+console.log("Ok")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
